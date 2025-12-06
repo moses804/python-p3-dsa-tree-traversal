374a3d565e549747159b968641723e0fb5d2cf5a
Moses Maina <mainacalton57@gmail.com>
2025-12-06 07:44:04 +0300

    lab complete

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,21 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, target_id):
+        # Start search from the root node
+        return self._dfs(self.root, target_id)
+
+    def _dfs(self, node, target_id):
+        # Base case: if this node has the id we want, return it
+        if node['id'] == target_id:
+            return node
+        
+        # Otherwise, recursively search in its children
+        for child in node['children']:
+            found = self._dfs(child, target_id)
+            if found:
+                return found
+        
+        # If nothing was found below, return None
+        return None
